3f20624a5912812a8e68a1c15b6f3a3d0148cad9
Paul Uchenna <pauluchenna20@gmail.com>
2024-08-07 17:08:42 +0100

    Active state

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Style the mortgage type checkboxes when clicked
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
   checkboxes.forEach(function (checkbox) {
@@ -12,4 +13,26 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  // Add focus styles to text inputs
+  document
+    .querySelectorAll(
+      ".input-mortgage-amount input, .input-mortgage-term input, .input-interest-rate input"
+    )
+    .forEach((input) => {
+      input.addEventListener("focus", () => {
+        input.parentElement.classList.add("focused");
+      });
+
+      input.addEventListener("blur", () => {
+        input.parentElement.classList.remove("focused");
+      });
+    });
+
+  // Cache DOM elements for future use
+  const clearBtn = document.getElementById("clear-btn");
+  const mortgageAmount = document.getElementById("mortgage-amount");
+  const mortgageTerm = document.getElementById("mortgage-term");
+  const interestRate = document.getElementById("interest-rate");
+  const btnCalculate = document.getElementById("btn-calculate");
 });
